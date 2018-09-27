df7c43dea7e94bde99f422548378d81ed530edbc
曾海明 <4041812@qq.com>
2018-09-27 17:41:05 +0800

    Update index.js

--- mobx初始运行环境/src/index.js
+++ mobx初始运行环境/src/index.js
@@ -1,9 +1,20 @@
-function decorators(target) {
-    target.color = 'black';
-}
-// 用@装饰器 装饰一个类
-@decorators
-class Car {
+import {observable, isArray, computed} from 'mobx';
 
+class Store {
+    @observable array = [];
+    @observable obj = {};
+    @observable map = new Map();
+
+    @observable string = 'hello';
+    @observable number = 20;
+    @observable bool = false;
 }
-console.log(Car.color);  
+
+var store = new Store();
+var foo = computed(()=>{ return store.string + '/' + store.number })
+
+console.log(foo)
+
+// const arr = observable(['a','b','c']);
+// console.log(arr)
+// document.write('hello')
